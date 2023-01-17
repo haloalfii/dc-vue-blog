5513cba97872640e6904374d4fe9b1a1484971f5
Alfian Luthfi <alfiankurniawan85@gmail.com>
2023-01-17 20:58:31 +0700

    Components Done

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
-    <div class="container mt-5">
-        <h1>Halo</h1>
+    <div>
+        <Navbar />
+        <blogWrapper />
     </div>
 </template>
+
+<script>
+import Navbar from "./components/partials/Navbar.vue";
+import blogWrapper from "./components/blogs/BlogWrapper.vue";
+
+export default {
+    components: { Navbar, blogWrapper },
+};
+</script>
---
+++ src/components/partials/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-dark navbar-dark bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand" href="#">Vue DC's Blog</a>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarText"
+                aria-controls="navbarText"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#"
+                            >Home</a
+                        >
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
